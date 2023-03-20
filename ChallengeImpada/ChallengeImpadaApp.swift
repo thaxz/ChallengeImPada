d6792719ee7c16ac1c19a7b1ea527dd5ed8a19cc
thaxz <thais.monteiroc@ufpe.br>
2023-03-20 17:16:33 -0300

    first commit

---
+++ ChallengeImpada/ChallengeImpadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeImpadaApp.swift
+//  ChallengeImpada
+//
+//  Created by thaxz on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeImpadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
