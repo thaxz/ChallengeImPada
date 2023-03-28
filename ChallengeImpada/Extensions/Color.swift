735fbbf80d99e41c66e9602ca299d2a20b7b6b40
thaxz <thais.monteiroc@ufpe.br>
2023-03-28 15:12:25 -0300

    feature(Colors): adding colors

---
+++ ChallengeImpada/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  ChallengeImpada
+//
+//  Created by thaxz on 28/03/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let primary = Color("PrimaryColor")
+    let secondary = Color("SecondaryColor")
+    
+}
