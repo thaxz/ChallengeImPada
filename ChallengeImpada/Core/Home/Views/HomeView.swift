9a10da8dd7c12ccf90be7e9700c5acca7e5836f9
CynaraCosta <cvocac@cin.ufpe.br>
2023-03-28 16:54:06 -0300

    feature(HomeView): created a extensions file for homeview components

--- ChallengeImpada/Core/Home/Views/HomeView.swift
+++ ChallengeImpada/Core/Home/Views/HomeView.swift
@@ -17,46 +17,7 @@ struct HomeView: View {
                 .ignoresSafeArea()
             
             // content layer
-            
-            VStack (spacing: 64) {
-                VStack (spacing: 80) {
-                    
-                    Text("ARQUIS")
-                        .frame(width: UIScreen.main.bounds.width * 0.43, height: UIScreen.main.bounds.height * 0.2)
-                        .font(.system(size: 100, weight: .bold))
-                        .foregroundColor(Color.theme.accent)
-                    
-                    Image("playButton")
-                    
-                    Button(action: standBy, label: {
-                        Text("Museu")
-                            .foregroundColor(Color.theme.primary)
-                            .font(.system(size: 50, weight: .bold))
-                            .frame(width: UIScreen.main.bounds.width * 0.2, height: UIScreen.main.bounds.height * 0.09)
-                            .background(Color.theme.accent)
-                            .cornerRadius(6)
-                    })
-  
-                }
-                
-                HStack {
-                    
-                    Button(action: standBy, label: {
-                        Text("Créditos")
-                            .foregroundColor(Color.theme.accent)
-                            .font(.system(size: 50, weight: .bold))
-                            .frame(width: UIScreen.main.bounds.width * 0.2, height: UIScreen.main.bounds.height * 0.09)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 6)
-                                    .stroke(Color.theme.accent, lineWidth: 5)
-                            )
-                            
-                    })
-                    Spacer()
-                }
-                .padding(.leading, 48)
-             
-            }
+            homeBody
   
         }
         
