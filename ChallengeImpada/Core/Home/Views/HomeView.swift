c72850dda90b4b71a213638e6b8da76f04fe5d10
CynaraCosta <cvocac@cin.ufpe.br>
2023-03-28 16:49:09 -0300

    feature(HomeView): created the HomeView

--- ChallengeImpada/Core/Home/Views/HomeView.swift
+++ ChallengeImpada/Core/Home/Views/HomeView.swift
@@ -9,8 +9,63 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            // background layer
+            Color.theme.primary
+                .ignoresSafeArea()
+            
+            // content layer
+            
+            VStack (spacing: 64) {
+                VStack (spacing: 80) {
+                    
+                    Text("ARQUIS")
+                        .frame(width: UIScreen.main.bounds.width * 0.43, height: UIScreen.main.bounds.height * 0.2)
+                        .font(.system(size: 100, weight: .bold))
+                        .foregroundColor(Color.theme.accent)
+                    
+                    Image("playButton")
+                    
+                    Button(action: standBy, label: {
+                        Text("Museu")
+                            .foregroundColor(Color.theme.primary)
+                            .font(.system(size: 50, weight: .bold))
+                            .frame(width: UIScreen.main.bounds.width * 0.2, height: UIScreen.main.bounds.height * 0.09)
+                            .background(Color.theme.accent)
+                            .cornerRadius(6)
+                    })
+  
+                }
+                
+                HStack {
+                    
+                    Button(action: standBy, label: {
+                        Text("Créditos")
+                            .foregroundColor(Color.theme.accent)
+                            .font(.system(size: 50, weight: .bold))
+                            .frame(width: UIScreen.main.bounds.width * 0.2, height: UIScreen.main.bounds.height * 0.09)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color.theme.accent, lineWidth: 5)
+                            )
+                            
+                    })
+                    Spacer()
+                }
+                .padding(.leading, 48)
+             
+            }
+  
+        }
+        
     }
+    
+    func standBy(){
+        
+    }
+    
 }
 
 struct HomeView_Previews: PreviewProvider {
