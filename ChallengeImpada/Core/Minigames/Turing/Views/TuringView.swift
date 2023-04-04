5cf7d9df0d9f5f23493b5854d452c6c470400102
CynaraCosta <cvocac@cin.ufpe.br>
2023-04-04 17:43:01 -0300

    feature(feature/TuringView): Turing's model, viewmodel and view

--- ChallengeImpada/Core/Minigames/Turing/Views/TuringView.swift
+++ ChallengeImpada/Core/Minigames/Turing/Views/TuringView.swift
@@ -1,21 +1,89 @@
-//
-//  TuringView.swift
-//  ChallengeImpada
-//
-//  Created by thaxz on 28/03/23.
-//
-
 import SwiftUI
 
+
 struct TuringView: View {
+    
+    @State var symbolsViewModel = SymbolViewModel()
+    @State var selectedFourSymbols = false
+    
     var body: some View {
         ZStack {
+            
+            let symbols = symbolsViewModel.sortSymbols()
+            // array with all the pressed buttons
+            var pressedSymbols: [Symbol] = []
+            
             Color.theme.primary
                 .ignoresSafeArea()
+            
             VStack {
-                Text("Oi sou o Turing")
-                    .foregroundColor(.white)
+                
+                HStack {
+                    ForEach(symbols, id: \.self) { symbol in
+                        symbol.symbolImage
+                            .resizable()
+                            .scaledToFit()
+                    }
+                }
+                
+                HStack {
+                    
+                    Button(action: {
+                        pressedSymbols.append(symbolsViewModel.symbols[0])
+                        if pressedSymbols.count == 4 {
+                            let result = symbolsViewModel.compareTwoArrayBySymbol(sortedArray: symbols, pressedArray: pressedSymbols)
+                            print(result)
+                        }
+                    }) {
+                        Image.theme.starButton
+                            .resizable()
+                    }
+                    
+                    
+                    Button(action: {
+                        pressedSymbols.append(symbolsViewModel.symbols[2])
+                        if pressedSymbols.count == 4 {
+                            let result = symbolsViewModel.compareTwoArrayBySymbol(sortedArray: symbols, pressedArray: pressedSymbols)
+                            print(result)
+                        }
+                    }) {
+                        Image.theme.squareButton
+                            .resizable()
+                    }
+                    
+                    
+                }
+                
+                HStack {
+                    
+                    Button(action: {
+                        pressedSymbols.append(symbolsViewModel.symbols[1])
+                        if pressedSymbols.count == 4 {
+                            let result = symbolsViewModel.compareTwoArrayBySymbol(sortedArray: symbols, pressedArray: pressedSymbols)
+                            print(result)
+                        }
+                    }) {
+                        Image.theme.circleButton
+                            .resizable()
+                    }
+                    
+                    
+                    Button(action: {
+                        pressedSymbols.append(symbolsViewModel.symbols[3])
+                        if pressedSymbols.count == 4 {
+                            let result = symbolsViewModel.compareTwoArrayBySymbol(sortedArray: symbols, pressedArray: pressedSymbols)
+                            print(result)
+                        }
+                        
+                    }) {
+                        Image.theme.triangleButton
+                            .resizable()
+                    }
+                    
+                }
+                
             }
+
         }
     }
 }
