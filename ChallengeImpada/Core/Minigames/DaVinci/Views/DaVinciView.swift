8448a54359661bf7aabe98f0ac33ff3094bcb139
Lucas Souza <lss8@cin.ufpe.br>
2023-04-11 20:58:00 -0300

    feature(minigameDaVinci): Modificações no codigo

--- ChallengeImpada/Core/Minigames/DaVinci/Views/DaVinciView.swift
+++ ChallengeImpada/Core/Minigames/DaVinci/Views/DaVinciView.swift
@@ -15,106 +15,107 @@ struct DaVinciView: View {
     }
     
     var body: some View {
-        ZStack {
-            //Color.theme.primary
-              //  .ignoresSafeArea()
-            VStack(spacing: 128) {
-                HStack(spacing: 128) {
-                    Button {
-                        DaVinciVM.redPress()
-                    } label: {
-                        RedButtonComponent
-                    }
-                    Button {
-                        DaVinciVM.bluePress()
-                    } label: {
-                        BlueButtonComponent
-                    }
-                    Button {
-                        DaVinciVM.yellowPress()
-                    } label: {
-                        YellowButtonComponent
-                    }
-                }
-                if DaVinciVM.secondStage {
-                    HStack(spacing: 128) {
-                        Button {
-                            DaVinciVM.greenPress()
-                        } label: {
-                            GreenButtonComponent
-                        }
-                        Button {
-                            DaVinciVM.purplePress()
-                        } label: {
-                            PurpleButtonComponent
-                        }
-                        Button {
-                            DaVinciVM.orangePress()
-                        } label: {
-                            OrangeButtonComponent
-                        }
-                    }
-                }
-                HStack {
-                    Button {
-                        DaVinciVM.merge()
-                        DaVinciVM.compareColors()
-                        DaVinciVM.removeFirstColor()
-                        DaVinciVM.removeSecondColor()
-                    } label: {
-                        mergeButtonComponent
-                    }
-                }
-                HStack(spacing: 128.0) {
-                    goalColor
-                        .onTapGesture {
-                            DaVinciVM.makeRandomColor()
-                        }
-                    HStack {
-                        ZStack {
-                            circleColor1
+            ZStack {
+                NavigationLink(destination: LevelSelectionView(), isActive: $DaVinciVM.backToSelection) {EmptyView()}
+                VStack(spacing: 128) {
+                    if !DaVinciVM.secondStage {
+                        HStack(spacing: 112) {
                             Button {
-                                DaVinciVM.removeFirstColor()
+                                DaVinciVM.redPress()
                             } label: {
-                                redoButton
+                                RedButtonComponent
+                            }
+                            Button {
+                                DaVinciVM.bluePress()
+                            } label: {
+                                BlueButtonComponent
+                            }
+                            Button {
+                                DaVinciVM.yellowPress()
+                            } label: {
+                                YellowButtonComponent
                             }
                         }
-                    }
-                    HStack {
-                        ZStack {
-                            circleColor2
+                        .padding(.horizontal)
+                        .offset(x: -12.0, y: 30.0)
+                    } else {
+                        HStack(spacing: 110.0) {
                             Button {
-                                DaVinciVM.removeSecondColor()
+                                DaVinciVM.redPress()
                             } label: {
-                                redoButton
+                                RedButtonComponent
+                            }
+                            Button {
+                                DaVinciVM.bluePress()
+                            } label: {
+                                BlueButtonComponent
+                            }
+                            Button {
+                                DaVinciVM.yellowPress()
+                            } label: {
+                                YellowButtonComponent
+                            }
+                            Button {
+                                DaVinciVM.greenPress()
+                            } label: {
+                                GreenButtonComponent
+                            }
+                            Button {
+                                DaVinciVM.purplePress()
+                            } label: {
+                                PurpleButtonComponent
+                            }
+                            Button {
+                                DaVinciVM.orangePress()
+                            } label: {
+                                OrangeButtonComponent
                             }
                         }
-                    }
+                        .offset(x: 0.0, y: 10.0)
                 }
-                
+                VStack(spacing: 128.0){
+                    Button {
+                    } label: {
+                        goalColor
+                    }
+                    Button {
+                        DaVinciVM.removeColors()
+                    } label: {
+                        redoButton
+                    }
+                    .offset(x: -0.0, y: -96.0)
+                }
+                .offset(x: -514.0, y: 170.0)
             }
+            }
+            .background(
+                DaVinciVM.bgImage
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            )
+            .onAppear{
+                DaVinciVM.makeRandomColor()
+            }
+            .sheet(isPresented: $DaVinciVM.tutorialPopUp) {
+                DaVinciTutorialView()
+                    .clearModalBackground()
+            }
+            .sheet(isPresented: $DaVinciVM.switchStagePopUp) {
+                SecondStageView()
+                    .clearModalBackground()
+            }
+            /*.sheet(isPresented: $DaVinciVM.endGamePopUp) {
+                EndGameView()
+                    .clearModalBackground()
+            }*/
+            .sheet(isPresented: $DaVinciVM.endGamePopUp, onDismiss: {
+                DaVinciVM.backToSelection.toggle()
+            }, content: {
+                EndGameView()
+                    .clearModalBackground()
+            })
         }
-        .onAppear{
-            DaVinciVM.makeRandomColor()
-        }
-        .sheet(isPresented: $DaVinciVM.tutorialPopUp) {
-            DaVinciTutorialView()
-                .clearModalBackground()
-                //.ignoresSafeArea()
-                
-        }
-        .sheet(isPresented: $DaVinciVM.switchStagePopUp) {
-            SecondStageView()
-                .clearModalBackground()
-                //.ignoresSafeArea()
-                
-        }
-        .sheet(isPresented: $DaVinciVM.endGamePopUp) {
-            EndGameView()
-                .clearModalBackground()
-                //.ignoresSafeArea()
-        }
-    }
 }
 
 struct DaVinciView_Previews: PreviewProvider {
--- ChallengeImpada/Core/Minigames/DaVinci/Views/SecondStageView.swift
+++ ChallengeImpada/Core/Minigames/DaVinci/Views/SecondStageView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SecondStageView: View {
     var body: some View {
-        Text("APARECI CARALHO")
+        Image("SecondView")
     }
 }
 
